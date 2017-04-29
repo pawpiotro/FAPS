2933a189c1764899a1b87e0b3a66db2832d16f96
Pawel Piotrowski <pawpiotro22@gmail.com>
2017-04-29 20:42:39 +0200

    Let the rock off begin

---
+++ ProxyServer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace FAPS
+{
+    class ProxyServer
+    {
+
+        // Incoming data from the client.  
+        public static string data = null;
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.  
+            // Dns.GetHostName returns the name of the   
+            // host running the application.  
+            /*IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            for(int i = 0; i < ipHostInfo.AddressList.Length; i++)
+            {
+                Console.WriteLine(ipHostInfo.AddressList[i] + "\n");
+            }
+            IPAddress ipAddress = ipHostInfo.AddressList[0];*/
+            IPAddress ipAddress = IPAddress.Parse("192.168.0.16");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and   
+            // listen for incoming connections.  
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.  
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.  
+                    while (true)
+                    {
+                        //TU SIE ZROBI AUTENTYKEJSZYN
+
+                        //A TU SIE ZROBI FORKA
+
+                        bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.  
+                    Console.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+
+        public static Boolean authenticate(String login, String pass)
+        {
+            var list = new List<Tuple<String, String>>();
+            String[] result;
+            String[] separators = { ";" };
+            using (StreamReader fs = new StreamReader("pass.txt"))
+            {
+                String line = null;
+                while ((line = fs.ReadLine()) != null)
+                {
+                    result = line.Split(separators,StringSplitOptions.RemoveEmptyEntries);
+                    list.Add(Tuple.Create<String, String>(result[0], result[1]));
+                }
+            }
+            /*
+            for(int i = 0; i < list.Count(); i++)
+            {
+                Console.WriteLine("Login: {0} Pass XD: {1}", list[i].Item1, list[i].Item2);
+            }*/
+            if (list.Contains(Tuple.Create<String, String>(login, pass)))
+                return true;
+            else
+                return false;
+        }
+
+        public static int Main(String[] args)
+        {
+            if (authenticate("user1", "pass1"))
+                Console.WriteLine("elo");
+            else
+                Console.WriteLine("nie elo");
+            //StartListening();
+            Console.WriteLine("\nPress ENTER to exit...");
+            Console.Read();
+            return 0;
+        }
+
+    }
+}
