11ab87d7828ed15d8973ebc075b8fe5b3f12bc5f
DOMINIK PISKORZ <dompoczta3@gmail.com>
2017-05-01 19:40:48 +0200

    Scheduler Pseudocode; Added VS Solution

---
+++ FAPS/FAPS/ProxyServer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace FAPS
+{
+    class ProxyServer
+    {
+        private static Scheduler scheduler;
+        private static Monitor monitor;
+        // Incoming data from the client.  
+        public static string data = null;
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new Byte[1024];
+
+            /*IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            for(int i = 0; i < ipHostInfo.AddressList.Length; i++)
+            {
+                Console.WriteLine(ipHostInfo.AddressList[i] + "\n");
+            }
+            IPAddress ipAddress = ipHostInfo.AddressList[0];*/
+
+            IPAddress ipAddress = IPAddress.Parse("192.168.0.16");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and   
+            // listen for incoming connections.  
+            Socket handler = null;
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                
+                // Start listening for connections.
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    handler = listener.Accept();
+                    data = null;
+
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    if (data.IndexOf("Herro") > -1)
+                    {
+                        ClientHandler client = new ClientHandler(monitor, handler);
+                        Thread tmp = new Thread(client.run);
+                        tmp.Start();
+                    }
+                    else
+                        Console.WriteLine("No threads for ya");
+                    Console.WriteLine("Text received : {0}", data);
+                    
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+
+
+
+        public static int Main(String[] args)
+        {
+            monitor = new Monitor();
+            scheduler = new Scheduler(monitor);
+            StartListening();
+            Console.WriteLine("\nPress ENTER to exit...");
+            Console.Read();
+            return 0;
+        }
+
+    }
+}
---
+++ FAPS/FAPS/Scheduler.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAPS
+{
+    class Scheduler
+    {
+        Monitor monitor;
+        // ServerList
+        bool dwnloading;
+        int lastFrag, maxFrag;
+        //fifo failedFrags;
+        //bool SuccFrags[];
+
+        public Scheduler(Monitor _monitor)
+        {
+            monitor = _monitor;
+            monitor.print();
+            dwnloading = false;
+            /*
+            for (i in SuccFrags)
+                SuccFrags[i] = false;
+            */
+        }
+
+        public void Success(int fragment)
+        {
+            // thready beda to wywolywac przy pomyslnym pobraniu fragmentu
+            //SuccFrags[fragment] = true;
+        }
+
+        // Methods creating networking threads for specific purpose
+        private void Download(string file, int fragment, int server)
+        {
+            //create ServerHandler(download, file, fragment, this, sever)
+        }
+
+        private void Upload(string file, int server)
+        {
+            //create ServerHandler(upload, file, this)
+        }
+
+        private void Command(string cmd, int server)
+        {
+            //create ServerHandler(command, cmd, this)
+        }
+
+        public void run()
+        {
+            /*   wez komendy
+                zrob komendy
+                profit
+            */
+            while(true)
+            {/*
+                if (!dwnloading && monitor.dlReady())
+                {
+                    // Someone's waiting for a download, start dl
+                    dwnloading = true;
+                    lastFrag = 0;
+                    dlFile = monitor.dlfetch();
+                    maxFrag = monitor.dlsize();
+                    SuccFrags.setsize(maxFrag);
+                }
+                else if (dwnloading)
+                {
+                    // Check if file is downloaded
+                    if (lastFrag > maxFrag && failedFrag.empty())   // Any fragments left to download?
+                    {
+                        bool done = true;
+                        for (i in SuccFrags)
+                            if (SuccFrags[i] = false)
+                                done = false;
+                        if (done)   // Did every fragment finished downloading?
+                            dwnloading = false;
+                    }
+                    else
+                    {
+                        // Look through the servers list and start download form idle ones
+                        bool dlbusy = false;
+                        for (server in ServerList)
+                            if (!server.busy)
+                                if (!failedFrags.empty())   // At least one fragment has to be redownloaded
+                                {
+                                    Download(dlfile, failedFrags.fetch(), server);
+                                    server.busy = true;
+                                }
+                                else if (lastFrag <= maxFrag)
+                                {
+                                    Download(dlfile, lastFrag, server);
+                                    server.busy = true;
+                                    lastFrag++;
+                                }
+                     }
+                }
+                if (monitor.ulReady())
+                {
+                    // Upload file on every server;
+                    ulFile = monitor.ulfetch()
+                    for (server in ServerList)
+                        Upload(ulFile, server);
+                }
+                if (monitor.cmdReady())
+                    cmd = monitor.cmdfetch();
+                    if (cmd = FILELIST)     // Get the list from one server (since they all share same files)
+                        Command(monitor.cmdfetch(), ServerList[0]);
+                    else
+                        for (server in ServerList)      // rename, delete etc - pass to all servers
+                            Command(monitor.cmdfetch(), ServerList[server]);
+            */
+            }
+        }
+    }
+}
